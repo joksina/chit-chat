d646d993f14fac03d04113005b5ed3652e1a1f22
joksina <jksalaudeen@yahoo.com>
2016-01-04 13:25:03 -0800

    working on the index.html

--- server.js
+++ server.js
@@ -23,11 +23,43 @@ app.listen(port, '127.0.0.1');
 io.listen(app);
 
 //connecting the socket to emmit a welcome message
-io.socket.on('connection', function (data) {
+io.socket.on('connection', function (socket) {
   var id = crypto.randomBytes(20).toString('hex');
-  users.push({data: data, id: id, name: null});
+  users.push({socket: socket, id: id, name: null});
   socket.emit('welcome', { message: "Hello, Welcome to Chit Chat", id: id});
+  sendUser();
+  socket.on('send', function (data) {
+    if(data.username !== '') {
+      setUser(id, data.username);
+    } else {
+      return alert('please enter your username');
+    }
+    if(data.toUser !== '') {
+      _.each(users, function (user) {
+        if (user.id === data.toUser || user.id === data.fromUser) {
+          user.socket.emit('receive', data);
+        }
+      });
+    } else {
+      io.socket.emit('receive', data);
+    }
+  });
 });
 
+//send the users
+var sendUser = function() {
+  io.sockets.emit('users', _.map(users, function (user) {
+    return { id: user.id, name: user.username };
+  }));
+};
+//Creating a funtion to set users
+var setUser = function (id, name) {
+  _.each(users, function (user) {
+    if(user.id === id) {
+      user.username = name;
+      sendUser();
+    }
+  });
+};
 
 console.log('Server running at http://127.0.0.1:' + port);
