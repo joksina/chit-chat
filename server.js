d9a55d6023c8f31d62a256d667ebfd9ebacff755
joksina <jksalaudeen@yahoo.com>
2016-01-04 12:26:33 -0800

    connecting socket.io

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var http = require("http");
 var fs = require('fs');
+var crypto = require('crypto');
 var port = 4568;
+var users = [];
+var io = require('socket.io');
 
 var html = fs.readFileSync(__dirname + "/public/index.html", {encoding: 'utf8'});
 var css = fs.readFileSync(__dirname + "/public/styles.css", {encoding: 'utf8'});
@@ -17,4 +20,14 @@ var app = http.createServer(function (request, response) {
 
 //listen to the app
 app.listen(port, '127.0.0.1');
+io.listen(app);
+
+//connecting the socket to emmit a welcome message
+io.socket.on('connection', function (data) {
+  var id = crypto.randomBytes(20).toString('hex');
+  users.push({data: data, id: id, name: null});
+  socket.emit('welcome', { message: "Hello, Welcome to Chit Chat", id: id});
+});
+
+
 console.log('Server running at http://127.0.0.1:' + port);
